dbb9afbaac6bed4b5f82578a49d874027c633a4c
MXThanh <thanhmxph18759@fpt.edu.vn>
2023-11-18 01:02:50 +0700

    create photobox BE

---
+++ photobox/src/main/java/com/ptb/app/PhotoboxApplication.java
@@ -0,0 +1,13 @@
+package com.ptb.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoboxApplication.class, args);
+    }
+
+}
---
+++ photobox/src/test/java/com/ptb/app/PhotoboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptb.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
